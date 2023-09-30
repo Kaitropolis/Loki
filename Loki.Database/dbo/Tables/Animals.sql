b995a00b6b05cd36e99f5bc042b93072474079ed
Rio <riostockton@gmail.com>
2023-09-30 23:15:07 +0100

    Update database project to match schema

---
+++ Loki.Database/dbo/Tables/Animals.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[Animals] (
+    [Id]           INT                IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (MAX)     NOT NULL,
+    [Continents]   NVARCHAR (MAX)     NOT NULL,
+    [Habitat]      NVARCHAR (MAX)     NOT NULL,
+    [Food]         NVARCHAR (MAX)     NOT NULL,
+    [DateCreated]  DATETIMEOFFSET (7) DEFAULT (getutcdate()) NOT NULL,
+    [DateModified] DATETIMEOFFSET (7) NULL,
+    [DateDeleted]  DATETIMEOFFSET (7) NULL,
+    [IsDeleted]    BIT                DEFAULT (CONVERT([bit],(0))) NOT NULL,
+    CONSTRAINT [PK_Animals] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Animals_IsDeleted]
+    ON [dbo].[Animals]([IsDeleted] ASC);
+
